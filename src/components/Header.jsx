d73aea8d05e734693815cb1df8802fbccd9cedfd
Triggerd79 <parthkharbanda.pk776@gmail.con>
2024-10-13 21:39:24 +0530

    added navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+// src/components/Header.jsx
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+
+const Header = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <Link to="/">
+                    <img src="/logo.svg" alt="AniVerse" />
+                </Link>
+            </div>
+            <div id="search">
+                <div class="search-content">
+                    <form action="/search" autocomplete="off">
+                        <a href="/filter" class="filter-icon">Filter</a>
+                        <input type="text" class="form-control search-input" name="keyword" placeholder="Search anime..." required="" />
+                        <button type="submit" class="search-icon"> <FaSearch /></button>
+                    </form>
+                </div>
+            </div>
+            <div className="menu">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/movies">Movies</Link></li>
+                    <li><Link to="/tv-series">TV Series</Link></li>
+                    <li><Link to="/community">Community</Link></li>
+                </ul>
+            </div>
+            <div className="user-actions">
+
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
